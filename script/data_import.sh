d399cb02a75d0c1177bff82bfa15ce63b069f823
jiquan.long <jiquan.long@zilliz.com>
2019-10-11 17:02:30 +0800

    add install&&import data

---
+++ script/data_import.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+#需要提前将数据导入到 '/tmp'下面
+#create database user and import data
+IS_RUN=$(ps ax | grep "megawise_server" | wc -l)
+while [ $IS_RUN -le 1 ];do
+	sleep 1
+	IS_RUN=$(ps ax | grep "megawise_server" | wc -l)
+done
+
+IS_RUN=$(ps ax | grep "postgres" | wc -l)
+while [ $IS_RUN -le 1 ];do
+	sleep 1
+	IS_RUN=$(ps ax | grep "postgres" | wc -l)
+done
+
+sleep 1
+
+/megawise/bin/psql postgres <<EOF
+CREATE USER zilliz WITH PASSWORD 'zilliz';
+grant all privileges on database postgres to zilliz;
+drop extension if exists zdb_fdw;
+create extension zdb_fdw;
+EOF
+/megawise/bin/psql postgres -U zilliz <<EOF
+drop table if exists nyc_taxi;
+create table nyc_taxi(
+    vendor_id text,
+    tpep_pickup_datetime timestamp,
+    tpep_dropoff_datetime timestamp,
+    passenger_count int,
+    trip_distance float,
+    pickup_longitute float,
+    pickup_latitute float,
+    dropoff_longitute float,
+    dropoff_latitute float,
+    fare_amount float,
+    tip_amount float,
+    total_amount float
+    );
+copy nyc_taxi from '/tmp/nyc_taxi_data.csv'
+ WITH DELIMITER ',' csv header;
+select count(*) from nyc_taxi;
+EOF
---
+++ script/install_megawise.sh
@@ -0,0 +1,90 @@
+#!/bin/bash
+
+if [ $# -eq 1 ];then
+	dir_location=$1
+	megawise_tag=0.3.0-d091919-1679
+elif [ $# -eq 2 ];then
+	dir_location=$1
+	megawise_tag=$2
+else
+	echo "usage install_megawise.sh /path/to/data_dir "
+	exit -1
+fi
+
+echo "magawise_tag :" $megawise_tag
+
+docker pull zilliz/megawise:$megawise_tag
+
+if [ -d ${dir_location} ];then
+	rm -rf $dir_location
+fi
+
+mkdir $dir_location
+
+if [ -d ${dir_location} ];then
+    echo "data location :" $dir_location
+else
+    echo "can't create $dir_location"
+    exit -1
+fi
+
+cp data_import.sh /tmp
+megawise_image_id=$(docker images |grep "zilliz/megawise" | grep "$megawise_tag" \
+ |awk '{printf "%s\n",$3}')
+echo "megawise_image_id:" $megawise_image_id
+
+MEGAWISE_CNT=$(docker ps | grep $megawise_image_id | wc -l)
+
+if [ $MEGAWISE_CNT -ne 0 ];then
+	echo "megawise is running..."
+	exit 0
+fi
+
+mkdir ${dir_location}/conf
+
+wget -P ${dir_location}/conf https://raw.githubusercontent.com/Infini-Analytics/infini/master/config/db/chewie_main.yaml
+wget -P ${dir_location}/conf https://raw.githubusercontent.com/Infini-Analytics/infini/master/config/db/etcd.yaml
+wget -P ${dir_location}/conf https://raw.githubusercontent.com/Infini-Analytics/infini/master/config/db/megawise_config.yaml
+wget -P ${dir_location}/conf https://raw.githubusercontent.com/Infini-Analytics/infini/master/config/db/megawise_config_template.yaml
+wget -P ${dir_location}/conf https://raw.githubusercontent.com/Infini-Analytics/infini/master/config/db/render_engine.yaml
+wget -P ${dir_location}/conf https://raw.githubusercontent.com/Infini-Analytics/infini/master/config/db/scheduler_config_template.yaml
+wget -P /tmp https://raw.githubusercontent.com/Infini-Analytics/infini/master/sample_data/nyc_taxi_data.csv
+
+mkdir ${dir_location}/data
+mkdir ${dir_location}/server_data
+
+docker run --gpus all --shm-size 17179869184 \
+ -v ${dir_location}/conf:/megawise/conf  \
+ -v ${dir_location}/data:/megawise/data  \
+ -v ${dir_location}/server_data:/megawise/server_data  \
+ -v /tmp:/tmp  \
+ -p 5433:5432  \
+ -d  \
+ ${megawise_image_id}
+
+IS_RUN=$(docker ps | grep ${megawise_image_id} | wc -l)
+TRY_CNT=0
+while [ $IS_RUN -eq 0 ];do
+	sleep 1
+	IS_RUN=$(docker ps | grep ${megawise_image_id} | wc -l)
+	if [ $TRY_CNT -ge 60 ];then
+		echo "start megawise in docker failed, check config please..."
+		exit -1
+	fi
+	TRY_CNT=$[$TRY_CNT + 1]
+done
+
+echo "start megawise in docker"
+
+container_id=$(docker ps |grep ${megawise_image_id} |awk '{printf "%s\n",$1}')
+
+echo "copying example data into meagwise"
+docker exec -u megawise -it ${container_id} /tmp/data_import.sh
+echo "copy example data into megawise finished"
+
+echo "listen_addresses = '*'" >> ${dir_location}/data/postgresql.conf
+echo "host    all             all             0.0.0.0/0          password" >> ${dir_location}/data/pg_hba.conf
+
+docker restart ${container_id}
+
+echo "finished..."
